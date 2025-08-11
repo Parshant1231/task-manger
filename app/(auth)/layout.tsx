16127618a06c9cd6161243b84b100cf77029e890
Parshant <parshant0011pk@gmail.com>
2025-08-11 20:10:08 +0530

    update the auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,26 +5,27 @@ import Loading from "../loading";
 import { userContext } from "@/context/userContext";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-    const { authReady } = useUserAuth(); 
-    const { loading } = useContext(userContext);
+  const { authReady } = useUserAuth();
+  const { loading } = useContext(userContext);
   if (loading || !authReady) return <Loading />;
 
-
   return (
-    
-        <div className="flex">
-          {/* Form container */}
-          <div className="w-screen min-h-screen md:w-[60vw] bg-white  px-12 pt-8 pb-12 ">
-            <h2 className="text-2xl font-semibold">Task Manager</h2>
-            {children}
+    <div className="flex">
+      {/* Form container */}
+      <div className="w-screen min-h-screen md:w-[60vw] bg-white  px-12 pt-8 pb-12 ">
+        <h2 className="text-2xl font-semibold">Task Manager</h2>
+        {children}
+      </div>
 
-          </div>
+      {/* Image container */}
+<div className="hidden md:flex w-[40vw] h-screen bg-blue-500 border-1 items-start justify-center">
+  <img
+    src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/ea5d0476339699.5c6694d453222.gif"
+    alt="Animated GIF"
+    className="w-full h-auto"
+  />
+</div>
 
-          {/* Image container */}
-          <div className="hidden md:flex w-[40vw] h-screen bg-blue-600 items-center justify-center">
-            <div className="w-64 lg:w-[90%]"></div>
-          </div>
-        </div>
-
+    </div>
   );
 }
